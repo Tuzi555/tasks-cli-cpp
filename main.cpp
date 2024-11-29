67d57daccbd1940fb4410c3e40136a264df2e821
tuzi555 <jakub.tuzar@gmail.com>
2024-11-29 12:52:54 +0100

    add input validation for add operation

--- main.cpp
+++ main.cpp
@@ -8,29 +8,44 @@ const std::string DELETE = "delete";
 const std::string LIST = "list";
 const std::string MARK_DONE = "mark-done";
 const std::string MARK_IN_PROGRESS = "mark-in-progress";
-
-void printToConsole(std::string textToPrint) {
-    std::cout << textToPrint << std::endl;
-}
+const std::string VALID_COMMANDS_NOTICE =
+    fmt::format("Specify a valid operation you want to make.\nValid "
+                "commands are:\n\n\t{}\n\t{}\n\t{}\n\t{}\n\t"
+                "{}\n\t{}",
+                ADD, UPDATE, DELETE, LIST, MARK_IN_PROGRESS, MARK_DONE);
 
 int main(int argc, char *argv[]) {
+
+    if (argc == 1) {
+        std::cout << VALID_COMMANDS_NOTICE << std::endl;
+        return 1;
+    }
+
     std::string mode = argv[1];
+
     if (mode == ADD) {
-        printToConsole("Task added successfully (ID: 1)");
+        if (argc < 3) {
+            fmt::println("Please provide a name for a task you want to add.");
+            return 1;
+        }
+        if (argc > 3) {
+            fmt::println(
+                "Please provide a name for the task inside the double quotes.");
+            return 1;
+        }
+        fmt::println("Task added successfully (ID: 1) \"{}\"", argv[2]);
     } else if (mode == UPDATE) {
-        printToConsole("Task updated");
+        fmt::println("Task updated");
     } else if (mode == DELETE) {
-        printToConsole("Task deleted");
+        fmt::println("Task deleted");
     } else if (mode == LIST) {
-        printToConsole("Here are your tasks");
+        fmt::println("Here are your tasks");
     } else if (mode == MARK_DONE) {
-        printToConsole("Task completed");
+        fmt::println("Task completed");
     } else if (mode == MARK_IN_PROGRESS) {
-        printToConsole("Task mark as in progress");
+        fmt::println("Task mark as in progress");
     } else {
-        fmt::println("Valid commands are \"{}\", \"{}\", \"{}\", \"{}\", "
-                     "\"{}\", \"{}\"",
-                     ADD, UPDATE, DELETE, LIST, MARK_IN_PROGRESS, MARK_DONE);
+        std::cout << VALID_COMMANDS_NOTICE << std::endl;
     }
     return 0;
 }
